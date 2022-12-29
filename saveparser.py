84c08b1c8eb15dfc6d0c35122b5735138b68bacb
Blaze Marshall <wertercatt@wertercatt.com>
2022-12-29 01:48:46 -0500

    exp

--- saveparser.py
+++ saveparser.py
@@ -864,6 +864,7 @@ with open(sys.argv[1], "rb") as savFile:
         tempDict["move3"] = moveNames[int.from_bytes(save[idx][4], byteorder="big")]
         tempDict["move4"] = moveNames[int.from_bytes(save[idx][5], byteorder="big")]
         tempDict["id"] = int.from_bytes(save[idx][6], byteorder="big")
+        tempDict["exp"] = int.from_bytes(save[idx][7], byteorder="big")
         tempDict["caughtLocation"] = landmarkConstants[int.from_bytes(save[idx][22], byteorder="big") - 1]
         boxPokemon.append(tempDict)
     print(json.dumps(boxPokemon, sort_keys=True, indent=4))
